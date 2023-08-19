5cb01d2a2c9170bc18d2767b0d61cee659a312b4
Mightypants <lbwanghr@icloud.com>
2023-08-19 23:01:47 +0800

    Initial Commit

---
+++ MacOne/MacOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOneApp.swift
+//  MacOne
+//
+//  Created by Haoran Wang on 2023/8/19.
+//
+
+import SwiftUI
+
+@main
+struct MacOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
